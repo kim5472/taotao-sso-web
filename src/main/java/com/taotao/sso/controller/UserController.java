f2de71736c76d3cbbd9951910a229554923c3bee
kim5472 <kimcyton@163.com>
2017-10-21 22:33:14 +0800

    完成login接口

--- src/main/java/com/taotao/sso/controller/UserController.java
+++ src/main/java/com/taotao/sso/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.taotao.sso.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.taotao.common.utils.CookieUtils;
 import com.taotao.common.utils.TaotaoResult;
 import com.taotao.pojo.TbUser;
 import com.taotao.sso.service.UserService;
@@ -22,6 +27,9 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@Value("${TOKEN_KEY}")
+	private String TOKEN_KEY;
+	
 	@RequestMapping("/user/check/{param}/{type}")
 	@ResponseBody
 	public TaotaoResult checkUserDate(
@@ -35,6 +43,20 @@ public class UserController {
 	@ResponseBody
 	public TaotaoResult register(TbUser user){
 		TaotaoResult result = userService.register(user);
+		return result;
+	}
+	
+	@RequestMapping(value="/user/login",method=RequestMethod.POST)
+	@ResponseBody
+	public TaotaoResult login(
+			String username,
+			String password,
+			HttpServletRequest request,
+			HttpServletResponse response){
+		TaotaoResult result = userService.login(username, password);
+		// 把token写入cookie
+		CookieUtils.setCookie(request, response, TOKEN_KEY, result.getData().toString());
+		
 		return result;
 	}
 	
