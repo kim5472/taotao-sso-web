e7404f37132455f3163e2b7c322954281fd5c620
kim5472 <kimcyton@163.com>
2017-10-25 09:49:26 +0800

    重定向于login.jsp中。

--- src/main/java/com/taotao/sso/controller/PageController.java
+++ src/main/java/com/taotao/sso/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.taotao.sso.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -17,7 +18,9 @@ public class PageController {
 	}
 	
 	@RequestMapping("/page/login")
-	public String showLogin(){
+	public String showLogin(String url,Model model){
+		// login.jsp中需要重定向的参数，返回给jsp文件一个redirect值
+		model.addAttribute("redirect", url);
 		return "login";
 	}
 }
